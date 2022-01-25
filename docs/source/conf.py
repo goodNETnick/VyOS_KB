39c449284bf6be6fb1f83c44ff0743c7b10d526b
goodNETnick <33053932+goodNETnick@users.noreply.github.com>
2022-01-25 14:02:19 +1000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'goodNETnick_VyOS_KB'
+copyright = '2021, goodNETnick'
+author = 'goodNETnick'
 
 release = '0.1'
 version = '0.1.0'
